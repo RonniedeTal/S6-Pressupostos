ed60c5db461ffea53897f8a202af2f910394cb8e
unknown <ronnievelz@gmail.com>
2025-04-18 23:30:47 +0200

    Language counter working through budgetService

--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -3,25 +3,45 @@ import { Injectable } from "@angular/core";
     providedIn:'root',
 })
 export class BudgetService {
-    pageCounters: { [key: string]: number } = {
-        Seo: 1,
-        Ads: 1,
-        Web: 1
-      };
-    
-      plusPages(service: string) {
-        if (this.pageCounters[service] < 99) {
-          this.pageCounters[service]++;
-        }
-      }
-    
-      minusPages(service: string) {
-        if (this.pageCounters[service] > 1) {
-          this.pageCounters[service]--;
-        }
-      }
-    
-      getPageCount(service: string): number {
-        return this.pageCounters[service];
-      }
+  pageCounters: { [key: string]: number } = {
+    Seo: 0,
+    Ads: 0,
+    Web: 0
+  };
+
+  plusPages(service: string) {
+    if (this.pageCounters[service] <10) {
+      this.pageCounters[service]++;
     }
+  }
+
+  minusPages(service: string) {
+    if (this.pageCounters[service] > 0) {
+      this.pageCounters[service]--;
+    }
+  }
+
+  getPageCount(service: string): number {
+    return this.pageCounters[service];
+  }
+
+  languagesCounters:{[key:string]:number}={
+    Seo: 0,
+    Ads: 0,
+    Web: 0
+  };
+  plusLanguages(service:string){
+    if(this.languagesCounters[service]<10)
+        this.languagesCounters[service]++
+  }
+
+  minusLanguages(service:string){
+    if(this.languagesCounters[service]>0){
+        this.languagesCounters[service]--
+    }
+  }
+
+  getLanguageCount(service: string): number {
+    return this.languagesCounters[service];
+  }
+}
