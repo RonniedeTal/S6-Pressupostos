e58fefc7da826e433dbb0bf05f237c3dabc56aa9
unknown <ronnievelz@gmail.com>
2025-04-18 19:15:01 +0200

    header created

--- src/app/section/card-section/card-section.component.ts
+++ src/app/section/card-section/card-section.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import {FormsModule} from '@angular/forms'
 @Component({
   selector: 'app-card-section',
-  imports: [CardComponent],
+  imports: [CardComponent, FormsModule],
   templateUrl: './card-section.component.html',
   styleUrl: './card-section.component.css'
 })
 export class CardSectionComponent {
+ selectedService: string = ''
+isChecked=false;
 
+checkBoxToggle(){
+  this.isChecked = !this.isChecked
+}
 }
