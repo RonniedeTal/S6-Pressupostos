8a956d35124dbd5294752ffa470aeb92de74a02f
unknown <ronnievelz@gmail.com>
2025-04-23 22:14:14 +0200

    getter and setter of the array created

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { UserInterface } from '../interfaces/user-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+users=signal<UserInterface []>([])
 
+getUser(){
+return this.users
+}
+setUser(user:UserInterface){
+  this.users.update(actualUsers=>[...actualUsers, user])
+}
   constructor() { }
 }
